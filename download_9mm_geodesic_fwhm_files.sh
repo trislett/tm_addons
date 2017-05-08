ccbcdd8fe2b131fac76c97150ca94cb4eca802ec
Tristram Lett <tris.lett@gmail.com>
2017-05-08 21:39:21 +0200

    fixed tail

--- download_9mm_geodesic_fwhm_files.sh
+++ download_9mm_geodesic_fwhm_files.sh
@@ -7,4 +7,5 @@ wget https://www.dropbox.com/s/fmh5eseua7fqdhc/lh_9.0mm_fwhm_distances.npy?dl=0
 wget https://www.dropbox.com/s/y23vfcizlwvnfhk/lh_9.0mm_fwhm_indices.npy?dl=0
 wget https://www.dropbox.com/s/0q6g65aak91j6b0/rh_9.0mm_fwhm_distances.npy?dl=0
 wget https://www.dropbox.com/s/pnbmazvf3mu9sfm/rh_9.0mm_fwhm_indices.npy?dl=0
+for i in *?dl=0; do mv $i $(basename $i ?dl=0); done
 echo "Done"
